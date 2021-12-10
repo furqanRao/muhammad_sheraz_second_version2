6d9239f4e8fa83286ad916b20b503c68851a98bc
sandean <sandeansal@gmail.com>
2021-12-10 17:37:19 +0500

    second2

--- muhammad_second_app/urls.py
+++ muhammad_second_app/urls.py
@@ -1,16 +1,16 @@
 from django.contrib import admin
 from django.urls import path
-from .views import *
+from . import views
 
 app_name = 'muhammad_second_app'
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', Logout.as_view(), name='logout'),
-    path('users/', GetUserView.as_view(), name='users'),
-    path('countries/', GetCountriesList.as_view(), name='countries'),
-    path('sales/', SalesView.as_view(), name='sales'),
-    path('sale_statistics/', SaleStatistics.as_view(), name='sale_statistics'),
-    path('upload_country_data/', UploadCountryData.as_view(), name='upload_country_data'),  # for adding country/city
-    path('upload_sales/', UploadProductSaleData.as_view(), name='upload_sales'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.Logout.as_view(), name='logout'),
+    path('users/', views.GetUserView.as_view(), name='users'),
+    path('countries/', views.GetCountriesList.as_view(), name='countries'),
+    path('sales/', views.SalesView.as_view(), name='sales'),
+    path('sale_statistics/', views.SaleStatistics.as_view(), name='sale_statistics'),
+    path('upload_country_data/', views.UploadCountryData.as_view(), name='upload_country_data'),  # for adding country/city
+    path('upload_sales/', views.UploadProductSaleData.as_view(), name='upload_sales'),
 ]
